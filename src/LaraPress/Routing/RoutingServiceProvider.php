2cbf2c49d74bec1d89cd17ad258dc5b673911de6
Jeff Berry <jeff@portonefive.com>
2016-03-01 10:26:12 -0700

    Update RoutingServiceProvider.php

--- src/LaraPress/Routing/RoutingServiceProvider.php
+++ src/LaraPress/Routing/RoutingServiceProvider.php
@@ -13,11 +13,14 @@ class RoutingServiceProvider extends BaseRoutingServiceProvider
      */
     protected function registerRouter()
     {
-        $this->app['router'] = $this->app->share(
-            function ($app)
-            {
-                return new Router($app['events'], $app['actions'], $app);
+        $this->app['router'] = $this->app->share(function ($app) {
+            return new Router($app['events'], $app['actions'], $app);
+        });
+
+        $this->app['actions']->listen('init', function() {
+            if (is_admin()) {
+                app('url')->forceRootUrl($_SERVER['WP_HOME']);
             }
-        );
+        });
     }
 }
