88d45636b6400cc83335efa6d39a2009e6921d23
Skyler Fenn <skylerfenn@users.noreply.github.com>
2018-02-08 14:52:18 -0700

    Allow DB_PORT in wp-config

--- src/publishes/public/wp-config.php
+++ src/publishes/public/wp-config.php
@@ -34,7 +34,7 @@ try {
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-define('DB_HOST', env('DB_HOST', 'localhost'));
+define('DB_HOST', env('DB_HOST', 'localhost') . ':' . env('DB_PORT', '3306'));
 define('DB_NAME', env('DB_DATABASE'));
 define('DB_USER', env('DB_USERNAME'));
 define('DB_PASSWORD', env('DB_PASSWORD'));
