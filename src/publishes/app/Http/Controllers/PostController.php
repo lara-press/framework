6ef7b7b90f94643118e371c36b3f2224db3da290
Skyler <skyler@portonefive.com>
2018-07-06 16:06:26 -0600

    Remove pagination render
    # Conflicts:
    #	src/publishes/app/Http/Controllers/PostController.php

--- src/publishes/app/Http/Controllers/PostController.php
+++ src/publishes/app/Http/Controllers/PostController.php
@@ -10,6 +10,22 @@ class PostController extends Controller
             return view('welcome');
         }
 
+        $paginator = null;
+
+        if (is_category()) {
+            $paginator = $this->posts->paginateByCategory(get_queried_object()->slug);
+        } elseif (is_archive()) {
+            $paginator = $this->posts->paginateByDate(wp_query()->query['year'], wp_query()->query['monthnum']);
+        } elseif (is_home()) {
+            $paginator = $this->posts->paginate();
+        }
+
+        if ($paginator) {
+            return view('welcome', [
+                'posts' => $paginator->items(),
+            ]);
+        }
+
         abort(404);
     }
 
